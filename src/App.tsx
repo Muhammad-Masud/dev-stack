530a9969ade26317d3a10a9cf108bf5fa374efc3
Masud004 <md.masud.rana.1997@outlook.com>
2026-09-16 09:07:51 +0600

    first commit-initial setup

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import Navbar from './components/Navbar.tsx'
+import Hero from './components/Hero.tsx'
+import TechnologiesSection from './components/TechnologiesSection.tsx'
+import Footer from './components/Footer.tsx'
+
+
